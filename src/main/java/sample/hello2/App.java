25a209241377c135ce4727ea10b51e051ea54342
Savatteri <giu.savatteri@gmail.com>
2020-05-27 14:03:43 +0200

    first commit

---
+++ src/main/java/sample/hello2/App.java
@@ -0,0 +1,13 @@
+package sample.hello2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
